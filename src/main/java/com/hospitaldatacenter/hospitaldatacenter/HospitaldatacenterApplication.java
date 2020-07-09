33193f87237fd709ba570d5c1f9357aec369e754
m176 <m17683857924@163.com>
2020-07-09 15:27:15 +0800

    HospitalDataCenter first upload

---
+++ src/main/java/com/hospitaldatacenter/hospitaldatacenter/HospitaldatacenterApplication.java
@@ -0,0 +1,13 @@
+package com.hospitaldatacenter.hospitaldatacenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitaldatacenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HospitaldatacenterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hospitaldatacenter/hospitaldatacenter/HospitaldatacenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hospitaldatacenter.hospitaldatacenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitaldatacenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
